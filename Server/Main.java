ba4e2643ce1843eb7554a990900b9856eaaa8ae2
Muimi272 <3039495749@qq.com>
2025-11-16 23:42:53 +0800

    Update Main.java

--- Server/Main.java
+++ Server/Main.java
@@ -6,20 +6,19 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Main {
     private static final CopyOnWriteArrayList<OutputStream> osl = new CopyOnWriteArrayList<>();
     private static final CopyOnWriteArrayList<String> allMemberNames = new CopyOnWriteArrayList<>();//allMemberNames是指的是在线的所有成员名字
-    private static final CopyOnWriteArrayList<String> legalMemberNames = new CopyOnWriteArrayList<>();//legalMemberNames是指的是所有能够通过登录验证的成员名字
-    private static final CopyOnWriteArrayList<String> legalMemberPasswords = new CopyOnWriteArrayList<>();
+    private static CopyOnWriteArrayList<User> legalMembers = new CopyOnWriteArrayList<>();//legalMembers是指的是所有能够通过登录验证的成员名字和密码
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(10000);
         System.out.println("服务器启动在端口 10000...");
         ChatLogger.creatPW();
         readLegalUsersInfo();
-        Admin.startAdminConsole(osl, allMemberNames, legalMemberNames, legalMemberPasswords);
+        Admin.startAdminConsole(osl, allMemberNames, legalMembers);
         while (true) {
             Socket clientSocket = serverSocket.accept();
             OutputStream myOS = clientSocket.getOutputStream();
             osl.add(myOS);
-            new ClientHandler(clientSocket, osl, myOS, allMemberNames, legalMemberNames, legalMemberPasswords).start();
+            new ClientHandler(clientSocket, osl, myOS, allMemberNames, legalMembers).start();
         }
     }
 
@@ -40,17 +39,12 @@ public class Main {
     }
 
     public static void readLegalUsersInfo() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("Members.json")));
-        StringBuilder sb = new StringBuilder();
-        int i;
-        while ((i = br.read()) != -1) {
-            if ((char) i == '\n') {
-                legalMemberNames.add(sb.toString().split(":")[0]);
-                legalMemberPasswords.add(sb.toString().split(":")[1]);
-                sb.setLength(0);
-            } else {
-                sb.append((char) i);
-            }
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("legalMembers.ser"));
+        try {
+            legalMembers = (CopyOnWriteArrayList<User>) ois.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
